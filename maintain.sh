56bad4a7da74fdf5e2961c2fac89a0b5fcc444af
John Sacco <johnsac@users.noreply.github.com>
2018-08-01 16:27:11 -0400

    Update maintain.sh

--- maintain.sh
+++ maintain.sh
@@ -15,10 +15,10 @@ printf "Example: su - gobyteadmin -c "~/gobyte/src/gobyted -daemon" "
 printf "Exact command for root user to start the node (see examples above): "
 read _startNode
 # Create a cronjob for making sure masternode runs after reboot
-if ! crontab -l | grep "@reboot ${_startNode}"; then
-  (crontab -l ; echo "@reboot ${_startNode}") | crontab -
+if ! crontab -l | grep "@reboot ${ _startNode; }"; then
+  (crontab -l ; echo "@reboot ${ _startNode; }") | crontab -
 fi
 # Create a cronjob for clearing the log file every two hours
-if ! crontab -l | grep "/bin/date > ~/.${_coinName}/debug.log"; then
-  (crontab -l ; echo "0 0 */2 * * /bin/date > ~/.${_coinName}/debug.log") | crontab -
+if ! crontab -l | grep "/bin/date > ~/.${ _coinName; }/debug.log"; then
+  (crontab -l ; echo "0 0 */2 * * /bin/date > ~/.${ _coinName; }/debug.log") | crontab -
 fi
